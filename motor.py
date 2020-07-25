79eab91f043968ee4b1cd480c133531e7c7f4030
sos010307 <sos010307@naver.com>
2020-07-25 15:46:11 +0900

    asdf

--- motor.py
+++ motor.py
@@ -1,36 +1,27 @@
-import RPi.GPIO as GPIO  
-from time import sleep
+import RPi.GPIO as GPIO
+import time
+ 
+pin = 19 # PWM pin num 18
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+p = GPIO.PWM(pin, 50)
+p.start(0)
+cnt = 0
+try:
+    while True:
+        p.ChangeDutyCycle(1)
+        print "angle : 1"
+        time.sleep(1)
+        p.ChangeDutyCycle(5)
+        print "angle : 5"
+        time.sleep(1)
+        p.ChangeDutyCycle(8)
+        print "angle : 8"
+        time.sleep(1)
+except KeyboardInterrupt:
+    p.stop()
+GPIO.cleanup()
 
 
-
-GPIO.setmode(GPIO.BOARD) 
-
-GPIO.setup(12, GPIO.OUT) 
-
-p = GPIO.PWM(12, 50)   
-
-p.start(0)            
-
-p.ChangeDutyCycle(3) 
-sleep(1)
-
-p.ChangeDutyCycle(12)
-sleep(1) 
-
-p.ChangeDutyCycle(7.5)
-sleep(1)
-
-
-
-while(1):
-
-  val = float(raw_input("input(3~7.5~12) = "))
-  
-  if val == -1: break
-
-  p.ChangeDutyCycle(val)
-  
-
-p.stop()                
-
-GPIO.cleanup() 
+출처: https://ljs93kr.tistory.com/40 [건프의 소소한 개발이야기]
